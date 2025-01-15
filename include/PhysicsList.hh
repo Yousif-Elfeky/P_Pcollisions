cc31fd403df4208b6e304656b4ce5f8563a6edc4
Yousif-Elfeky <yousifjoo@aucegypt.edu>
2025-01-15 23:53:48 +0200

    Update PhysicsList.hh

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -9,6 +9,10 @@
 #include "G4HadronPhysicsQGSP_BERT.hh"
 #include "G4DecayPhysics.hh"
 #include "G4HadronPhysicsFTFP_BERT_HP.hh"
+#include "G4HadronPhysicsQGSP_FTFP_BERT.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4RadioactiveDecay.hh"
+#include "G4StepLimiterPhysics.hh"
 
 
 
